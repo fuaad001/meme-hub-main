3ece4570f39a2c3ed38e973067e356bde5ae636a
Bettyrosew <bettyrosaw@gmail.com>
2019-03-07 15:55:49 +0300

    edit functions one, two, three and four for variable i.

--- js/scripts.js
+++ js/scripts.js
@@ -70,3 +70,31 @@ $(document).ready(function(){
   })
 });
 var elements = $(".column");
+var i;
+function one() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "100%";
+    elements[i].style.flex = "100%";
+  }
+}
+
+function two() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "50%";
+    elements[i].style.flex = "50%";
+  }
+}
+
+function three() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "33%";
+    elements[i].style.flex = "33%";
+  }
+}
+
+function four() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "25%";
+    elements[i].style.flex = "25%";
+  }
+}
